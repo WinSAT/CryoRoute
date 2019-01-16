c0ddbf8e0f25f7f6fce86abb73b4af1b112550fa
Atilla Saadat <atillaabc@gmail.com>
2019-01-16 11:02:36 -0500

    added line draw capabilities, prelim pathfinding alg

--- netcdfTest.py
+++ netcdfTest.py
@@ -13,11 +13,12 @@ nc.close()
 import geopy.distance, collections, heapq
 from math import cos, asin, sqrt
 
-Location = collections.namedtuple("Location", "lat lon iceThickness std numValues distCOG".split())
+Location = collections.namedtuple("Location", "ID lat lon iceThickness std numValues distCOG".split())
 data = {}
 
 newData = open("thk_28.map.complete.08122018_04012019.txt").read().split('\n')
 data = {}
+indexDict = {}
 lons = []
 lats = []
 th = []
@@ -25,9 +26,10 @@ th = []
 #endLocation = (x,y)
 for idx, d in enumerate(newData[1:]):
     try:
-        data[idx] = Location(*[float(i) for i in d.split(' ')])
+        data[idx] = Location(*[idx]+[round(float(i),4) for i in d.split(' ')])
         lons.append(data[idx].lon)
         lats.append(data[idx].lat)
+        indexDict[tuple(np.around([data[idx].lon,data[idx].lat],4))] = idx
         th.append(data[idx].iceThickness)
     except Exception as e: 
         print e,'Failed to store index {}: {}'.format(idx,d)
@@ -43,23 +45,78 @@ class LineBuilder:
         self.line = line
         self.clickedLat = []
         self.clickedLon = []
-        #self.xs = list(line.get_xdata())
-        #self.ys = list(line.get_ydata())
+        self.p1 = np.around([-168.611759,70.319223],4)
+        self.p2 = np.around([73.194979,82.825055],4)
+        self.startIndex = indexDict[tuple(self.p1)]
+        self.endIndex = indexDict[tuple(self.p2)]
+        pc1 = m(*self.p1)
+        pc2 = m(*self.p2)
+        self.xs = [pc1[0],pc2[0]]#list(line.get_xdata())
+        self.ys = [pc1[1],pc2[1]]#list(line.get_ydata())
         self.cid = line.figure.canvas.mpl_connect('button_press_event', self)
+        self.line.set_data(self.xs, self.ys)
+        self.line.set_color('red')
+        self.line.figure.canvas.draw()
+        #self.calcPath()
     
-    def formatCord(self,x,y):
-		return np.around(m(x,y,inverse=True))
+    def formatCord(self,lon,lat):
+		return m(lon,lat,inverse=True)
 
     def __call__(self, event):
     	currentLon, currentLat = self.formatCord(event.xdata, event.ydata)
         self.clickedLon.append(currentLon)
         self.clickedLat.append(currentLat)
-        print "clicked", self.clickedLat, self.clickedLon
+        print "clicked", currentLat, currentLon, m(currentLon,currentLat)
+        #from IPython import embed; embed()
+        #print checkValidPos(currentLat,currentLon)
         if event.inaxes!=self.line.axes: return
-        #self.xs.append(event.xdata)
-        #self.ys.append(event.ydata)
-        #self.line.set_data(self.xs, self.ys)
-        #self.line.figure.canvas.draw()
+        self.xs.append(event.xdata)
+        self.ys.append(event.ydata)
+        self.line.set_data(self.xs, self.ys)
+        self.line.figure.canvas.draw()
+    def drawLine(self,lineLats,lineLons):
+    	self.line.set_data(lineLats,lineLons)
+    	self.line.figure.canvas.draw()
+
+    def calcH(self, start, end):
+    	coords_1 = (data[start].lat, data[start].lon)
+    	coords_2 = (data[end].lat, data[end].lon)
+    	return (geopy.distance.distance(coords_1, coords_2)).km*(1+data[end].iceThickness)
+
+    def getNeigh(self, startlocation, n=10):
+    	return sorted(data.values(), key=lambda x: self.calcH(startlocation, x.ID))[1:n+1]
+
+    def getParent(self, closedlist, index):
+	    path = []
+	    while index is not None:
+	        path.append(index)
+	        index = closedlist.get(index, None)
+	    return [data[i] for i in path[::-1]]
+
+    def calcPath(self):
+    	Node = collections.namedtuple("Node", "ID F G H parentID".split())
+    	h = self.calcH(self.startIndex, self.endIndex)
+    	openlist = [(h, Node(self.startIndex, h, 0, h, None))] # heap
+    	closedlist = {} # map visited nodes to parent
+    	while len(openlist) >= 1:
+    		_, currentLocation = heapq.heappop(openlist)
+    		print(currentLocation)
+    		if currentLocation.ID in closedlist:
+    			continue
+    			closedlist[currentLocation.ID] = currentLocation.parentID
+
+    		if currentLocation.ID == self.endIndex:
+    			print("Complete")
+    			for p in self.getParent(closedlist, currentLocation.ID):
+    				print(p)
+    			break
+
+    		for other in self.getNeigh(currentLocation.ID):
+    			g = currentLocation.G + self.calcH(currentLocation.ID, other.ID)
+    			h = self.calcH(other.ID, self.endIndex)
+    			f = g + h
+    			heapq.heappush(openlist, (f, Node(other.ID, f, g, h, currentLocation.ID)))
+
 
 m = Basemap(projection='npstere',boundinglat=45,lon_0=270,resolution='l')
 # draw parallels and meridians.
